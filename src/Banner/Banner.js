e4c04c928a2f8baa4521bf93fb1901ecbc0b563a
roger <rogerfrontend@gmail.com>
2021-01-02 15:02:44 -0500

    List Orders

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Banner = styled.div`
     height: 400px;
-    background-image: url("img/banner.jpg");
+    background-image: linear-gradient(to top, #9890e3 0%, #b1f4cf 100%);
     background-position:center;
     background-size: cover;
     filter: contrast(75%);
--- src/Order/Order.js
+++ src/Order/Order.js
@@ -19,6 +19,14 @@ const OrderContent = styled(DialogContent)`
     height: 100%;
 `;
 
+const OrderContainer = styled.div`
+    padding: 10px 0px;
+    border-bottom: 1px solid grey;
+`
+
+const OrderItem = styled.div`
+    padding: 10px 0px;
+`
 
 export function Order({orders}){
     return (
@@ -27,7 +35,18 @@ export function Order({orders}){
                 orders.length === 0 ? 
                 (<OrderContent>Your order's lookinng pretty empty.</OrderContent>) 
                 : 
-                (<OrderContent> Found {orders.length} orders</OrderContent>)
+                (<OrderContent> <OrderContainer>Your Oder: </OrderContainer>
+                {orders.map(order => (
+                    <OrderContainer>
+                        <OrderItem>
+                            {order.name}
+                        </OrderItem>   
+                    </OrderContainer>    
+                ))
+
+                }
+                
+                </OrderContent>)
             } 
             <DialogFooter>
                 <ConfirmButton>Checkout</ConfirmButton>
