5a4f7a1df88f302d3fdd975453d6f593cb7556b3
roger <rogerfrontend@gmail.com>
2021-01-09 12:51:17 -0500

    Add total, tax and subtotal

--- src/Order/Order.js
+++ src/Order/Order.js
@@ -34,6 +34,11 @@ const OrderItem = styled.div`
 `
 
 export function Order({orders}){
+    const subtotal = orders.reduce((total, order)=>{
+        return total + getPrice(order);
+    },0);
+    const tax = subtotal * 0.07;
+    const total = subtotal + tax;
     return (
         <OrderStyled>
             {
@@ -50,10 +55,25 @@ export function Order({orders}){
                             <div>{formatPrice(getPrice(order))}</div>   
                         </OrderItem>   
                     </OrderContainer>    
-                ))
-
-                }
-                
+                ))}
+                    <OrderContainer>
+                        <OrderItem>
+                            <div/>
+                            <div>Sub total</div>
+                            <div>{formatPrice(subtotal)}</div>
+                        </OrderItem>
+                        <OrderItem>
+                            <div/>
+                            <div>Tax</div>
+                            <div>{formatPrice(tax)}</div>
+                        </OrderItem>
+                        <OrderItem>
+                            <div/>
+                            <div>Total</div>
+                            <div>{formatPrice(total)}</div>
+                        </OrderItem>
+                    </OrderContainer> 
+                    
                 </OrderContent>)
             } 
             <DialogFooter>
